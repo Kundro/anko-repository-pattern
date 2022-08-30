6b525eacbdb1d2fb910ba4e84fd8ac8ad0262377
Alexey Kundro <exeykundro@gmail.com>
2022-08-30 12:56:40 +0300

    ref: update customers validation

--- Customer.Datalayer/src/Customer.Datalayer/BusinessEntities/Customers.cs
+++ Customer.Datalayer/src/Customer.Datalayer/BusinessEntities/Customers.cs
@@ -7,16 +7,16 @@ namespace Customer.Datalayer.BusinessEntities
         public int CustomerID { get; set; }
         [StringLength(50, ErrorMessage = "First name should be less than 50.")]
         public string FirstName { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last name is required.")]
         [StringLength(50, ErrorMessage = "Last name should be less than 50.")]
         public string LastName { get; set; }
-        [RegularExpression("^\\+?\\d{6,7}[2-9]\\d{3}$", ErrorMessage = "Invalid phone number.")]
+        [RegularExpression("^\\+[1-9]\\d{1,14}$", ErrorMessage = "Invalid phone number.")]
         public string PhoneNumber { get; set; }
         [RegularExpression("^([\\w-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([\\w-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$", ErrorMessage = "Invalid email.")]
         public string Email { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "At least one note is required.")]
         public string Notes { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Total purchases amount is required.")]
         public decimal? TotalPurchasesAmount { get; set; }
     }
 }
