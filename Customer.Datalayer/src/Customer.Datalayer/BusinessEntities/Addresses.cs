72dcd12e5cac5599f07908e625fd0550846636a8
Alexey Kundro <exeykundro@gmail.com>
2022-09-01 11:08:47 +0300

    fix: update edit button in details page

--- Customer.Datalayer/src/Customer.Datalayer/BusinessEntities/Addresses.cs
+++ Customer.Datalayer/src/Customer.Datalayer/BusinessEntities/Addresses.cs
@@ -1,15 +1,27 @@
-﻿namespace Customer.Datalayer.BusinessEntities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Customer.Datalayer.BusinessEntities
 {
     public class Addresses
     {
         public int AddressID { get; set; }
         public int CustomerID { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Address line is required."), StringLength(100, ErrorMessage = "Address line length should be less than 100.")]
         public string AddressLine { get; set; } = string.Empty;
+        [StringLength(100, ErrorMessage = "Address line length should be less than 100.")]
+
         public string AddressLine2 { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Address type is required.")]
         public string AddressType { get; set; } = "Unknown";
+        [Required(AllowEmptyStrings = false, ErrorMessage = "City is required."), StringLength(50, ErrorMessage = "City name length should be less than 50.")]
         public string City { get; set; } = string.Empty;
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Postal code is required."), StringLength(6, ErrorMessage = "Postal code length should be 6.")]
+
         public string PostalCode { get; set; } = string.Empty;
+        [Required(AllowEmptyStrings = false, ErrorMessage = "State name is required."), StringLength(20, ErrorMessage = "State name length should be less than 20.")]
+
         public string StateName { get; set; } = string.Empty;
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Country is required.")]
         public string Country { get; set; } = string.Empty;
     }
 }
--- Customer.Datalayer/src/Customer.Datalayer/BusinessEntities/Customers.cs
+++ Customer.Datalayer/src/Customer.Datalayer/BusinessEntities/Customers.cs
@@ -7,8 +7,7 @@ namespace Customer.Datalayer.BusinessEntities
         public int CustomerID { get; set; }
         [StringLength(50, ErrorMessage = "First name should be less than 50.")]
         public string FirstName { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Last name is required.")]
-        [StringLength(50, ErrorMessage = "Last name should be less than 50.")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last name is required."), StringLength(50, ErrorMessage = "Last name should be less than 50.")]
         public string LastName { get; set; }
         [RegularExpression("^\\+[1-9]\\d{1,14}$", ErrorMessage = "Invalid phone number.")]
         public string PhoneNumber { get; set; }
