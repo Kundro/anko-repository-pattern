6fe8ee65d5879bb22e88daeaae8e3d25ceef5d41
Alexey Kundro <exeykundro@gmail.com>
2022-09-23 11:56:19 +0300

    feat: initial and configure web api project

---
+++ Customer.Datalayer/src/Customer.Datalayer.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using System.CodeDom;
+using Customer.Datalayer.BusinessEntities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.Datalayer.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Customers>>> Get()
+        {
+            var customers = new List<Customers>
+            {
+                new Customers
+                {
+                    CustomerId = 1,
+                    FirstName = "TestName",
+                    LastName = "TestLastName",
+                    Addresses = new List<Addresses>(),
+                    Email = "test@mail.ru",
+                    Notes = "TestNote",
+                    PhoneNumber = "+12341234567890",
+                    TotalPurchasesAmount = 1
+                }
+            };
+            return Ok(customers);
+        }
+    }
+}
--- Customer.Datalayer/src/Customer.Datalayer.WebApi/Program.cs
+++ Customer.Datalayer/src/Customer.Datalayer.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Customer.Datalayer.EFRepositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
